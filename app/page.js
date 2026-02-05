151ddf19ff1735343271aa2fa4cabf1c0d3c413f
oshadhaCperera <oshadhacperera999@gmail.com>
2026-02-05 19:21:51 +0530

    6th

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import { kv } from "@vercel/kv";
 
 export const runtime = "nodejs";
+export const dynamic = "force-dynamic"; // ⬅️ THIS FIXES THE ERROR
 
 export default async function Home() {
     const open = await kv.get("saloonOpen");
